bae66ef495b6f5269c35cb4f6f17d22271c765b3
Hieu Lai <hieuqlai@gmail.com>
2018-06-26 18:05:14 +1000

    style add recipe and display of recipes

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -6,20 +6,53 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import uuid from 'uuid';
+import PlusIcon from 'react-icons/lib/fa/plus-circle';
 import applyFilters from '../selectors/recipes';
 import RecipeDisplayCard from './RecipeDisplayCard';
-
-const RecipesList = (props) => (
-  <div className="content-container">
-    <div className="list-body">
-      {props.recipes.map((recipe) => (
-        <RecipeDisplayCard key={uuid()} recipe={recipe}/>
-      ))}
-      <Link className="card" to="/create">Add recipe</Link>
-      {console.log(props.recipes)}
-    </div>  
-  </div>
-);
+ 
+class RecipesList extends React.Component {
+  state = { 
+    filler: []
+  };
+  fillTheGapFunc = () => { 
+    const div = <div key={uuid()} className="card-add card--invis"></div>;
+    let numOfRecipes = this.props.recipes.length;
+    if (window.innerWidth >= 596 && window.innerWidth <= 870 && numOfRecipes%2 === 0) {
+      this.setState(() => ({ filler: [div] }));
+    } else if (window.innerWidth > 871 && numOfRecipes%3-1 === 0) {
+      this.setState(() => ({ filler: [div] }));
+    } else if (window.innerWidth > 871 && numOfRecipes%3 === 0) {
+      this.setState(() => ({ filler: [div, div] }));
+    } else {
+      this.setState(() => ({ filler: [] })); 
+    }
+  };  
+  componentDidMount () {
+    this.fillTheGapFunc();
+    window.addEventListener('resize', this.fillTheGapFunc);
+    window.addEventListener('keyup', this.fillTheGapFunc);
+  };
+  componentWillMount () {
+    window.removeEventListener('resize', this.fillTheGapFunc);
+    window.removeEventListener('keyup', this.fillTheGapFunc);
+  };
+  render() {
+    return (
+      <div className="content-container">
+        <div className="list-body">
+          {this.props.recipes.map((recipe) => (
+            <RecipeDisplayCard key={uuid()} recipe={recipe}/>
+          ))}
+          <Link className="card-add" to="/create">
+            <PlusIcon size={96} />
+          </Link>          
+          {this.state.filler.map(div => div)}
+        </div>  
+        {console.log(this.state.filler)}
+      </div>
+    );
+  };
+};
 
 const mapStateToProps = (state) => ({
   recipes: applyFilters(state.recipes, state.filters)
